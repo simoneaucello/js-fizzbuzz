c78d0a16f52af0df111b2bc357b380fbb0c61031
simoneaucello <simoneaucello@gmail.com>
2024-02-22 15:29:03 +0100

    [add] logic to js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-let i
+
+const list = document.getElementById('list')
 
 for(let i = 1; i <= 100; i++){
   console.log(i);
 
-  if(i % 3 === 0){
+  let li = document.createElement('li')
 
+  if((i % 3 === 0) && (i % 5 === 0)){
+    li.innerHTML = 'Fizz Buzz'
+    console.log('Fizz Buzz');
+
+  }else if(i % 5 === 0){
+    li.innerHTML = 'Buzz';
+    console.log('Buzz');
+
+  }else if (i % 3 === 0){
+    li.innerHTML = 'Fizz';
+    console.log('Fizz');
+
+  }else {
+    console.log(i);
+    li.innerHTML = i
   }
+
+  list.append(li)
+
 }
